8a59990e569a1bf16ae23636e672c2e69abafdc5
ricardojrloureiro@gmail.com <ricardojrloureiro@gmail.com>
2014-11-18 19:36:41 +0000

    lawl

--- src/Evaluator.cpp
+++ src/Evaluator.cpp
@@ -9,22 +9,22 @@ GLfloat ctrlpoints[4][3] = {	{  -0.5, 0.0, 0.5},
 								{  -0.5, 0.0, -0.5},
 								{ 0.5, 0.0, 0.5},
 								{ 0.5, 0.0, -0.5} };
-								*/
+ */
 
 GLfloat nrmlcompon[4][3] = {	{  0.0, 1.0, 0.0},
-								{  0.0, 1.0, 0.0},
-								{  0.0, 1.0, 0.0},
-								{  0.0, 1.0, 0.0} };
+		{  0.0, 1.0, 0.0},
+		{  0.0, 1.0, 0.0},
+		{  0.0, 1.0, 0.0} };
 
 GLfloat colorpoints[4][4] = {	{ 0.5, 0.5, 0.5, 0},
-								{ 0.5, 0.5, 0.5, 0},
-								{ 0.5, 0.5, 0.5, 0},
-								{ 0.5, 0.5, 0.5, 0} };
+		{ 0.5, 0.5, 0.5, 0},
+		{ 0.5, 0.5, 0.5, 0},
+		{ 0.5, 0.5, 0.5, 0} };
 
 GLfloat textpoints[4][2] = {	{ 0.0, 0.0},
-								{ 0.0, 1.0},
-								{ 1.0, 0.0},
-								{ 1.0, 1.0} };
+		{ 0.0, 1.0},
+		{ 1.0, 0.0},
+		{ 1.0, 1.0} };
 
 
 
@@ -45,16 +45,16 @@ void Evaluator::draw(){
 
 	//TODO GL_AUTO_NORMAL ? ver se é preciso
 
-	glMap2f(GL_MAP2_VERTEX_3, 0.0, 1.0, 3, degree,  0.0, 1.0, 6, 3,  ctrlpoints);
+	glMap2f(GL_MAP2_VERTEX_3, 0.0, 1.0, 3, degree,  0.0, 1.0, 12, 4,  ctrlpoints);
 	glMap2f(GL_MAP2_NORMAL,   0.0, 1.0, 3, degree,  0.0, 1.0, 6, 3,  &nrmlcompon[0][0]);
-	//glMap2f(GL_MAP2_COLOR_4,  0.0, 1.0, 4, degree,  0.0, 1.0, 8, 2,  &colorpoints[0][0]);
-	glMap2f(GL_MAP2_TEXTURE_COORD_2,  0.0, 1.0, 2, degree,  0.0, 1.0, 4, 2,  &textpoints[0][0]);
+	glMap2f(GL_MAP2_COLOR_4,  0.0, 1.0, 4, degree,  0.0, 1.0, 8, 2,  &colorpoints[0][0]);
+	glMap2f(GL_MAP2_TEXTURE_COORD_2,  0.0, 1.0, 2, 2,  0.0, 1.0, 4, 2,  &textpoints[0][0]);
 
 	glEnable(GL_MAP2_VERTEX_3);
 	glEnable(GL_MAP2_NORMAL);
-	//glEnable(GL_MAP2_COLOR_4);
+	glEnable(GL_MAP2_COLOR_4);
 	glEnable(GL_MAP2_TEXTURE_COORD_2);
-	glEnable(GL_AUTO_NORMAL);
+	//glEnable(GL_AUTO_NORMAL);
 
 	//TODO por dinamico aqui e no EvalMesh
 	glMapGrid2f(uPatches, 0.0,1.0, vPatches, 0.0,1.0);
